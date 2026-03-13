8deaae07d0ec101e77a9afb9a9696cd5f29a5f15
owen <owenthroup@gmail.com>
2026-03-13 22:14:35 +0000

    added unit tests to learn how to do them

--- src/headers/ethernet.rs
+++ src/headers/ethernet.rs
@@ -20,3 +20,36 @@ impl EthernetFrame {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn sample_ethernet_data() -> [u8; 14] {
+        [
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // dst mac
+            0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, // src mac
+            0x08, 0x00,  // ethertype IPv4
+        ]
+    }
+
+    #[test]
+    fn test_parse_ethertype() {
+        let eth_frame: EthernetFrame = EthernetFrame::parse(&sample_ethernet_data()).unwrap();
+        assert_eq!(eth_frame.ethertype, 0x0800);
+    }
+
+    #[test]
+    fn test_parse_mac_addresses() {
+        let eth_frame = EthernetFrame::parse(&sample_ethernet_data()).unwrap();
+        assert_eq!(eth_frame.dst_mac, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+        assert_eq!(eth_frame.src_mac, [0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c]);
+    }
+
+    #[test]
+    fn test_parse_not_enough_data() {
+        // first 10 bytes (needs 14 to not panic)
+        let data = &sample_ethernet_data()[..10];
+        assert!(EthernetFrame::parse(data).is_none());
+    }
+}
